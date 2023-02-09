54f1132cf8365b4ab701c4a0ce75dbcce8537f5c
Benjamin Cohen <bac1087@gmail.com>
2023-02-09 16:18:29 -0700

    updated armstrongnumbersspec to work and also have a more understandable helper fn

--- js/armstrongNumbersSpec.js
+++ js/armstrongNumbersSpec.js
@@ -1,11 +1,29 @@
-var arm = require("./armstrongNumbers");
-var shallowEqualArrays = require('shallow-equal/arrays');
+const arm = require("./armstrongNumbers");
+const shallowEqualArrays = require("shallow-equal").shallowEqualArrays;
 
 function createArrayOfNum(maxValue) {
-  return Array.apply(null, {length: maxValue}).map(Number.call, Number)
+  return [...Array(maxValue).keys()];
 }
 
-console.log(shallowEqualArrays(arm.findArmstrongNumbers([0]),[0]));
-console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(8)), [0, 1, 2, 3, 4, 5, 6, 7]));
-console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(99)), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(999)), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]));
+console.log(createArrayOfNum(8));
+console.log(createArrayOfNum2(8));
+
+console.log(shallowEqualArrays(arm.findArmstrongNumbers([0]), [0]));
+console.log(
+  shallowEqualArrays(
+    arm.findArmstrongNumbers(createArrayOfNum(8)),
+    [0, 1, 2, 3, 4, 5, 6, 7]
+  )
+);
+console.log(
+  shallowEqualArrays(
+    arm.findArmstrongNumbers(createArrayOfNum(99)),
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  )
+);
+console.log(
+  shallowEqualArrays(
+    arm.findArmstrongNumbers(createArrayOfNum(999)),
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]
+  )
+);
