c76fba40380bd87c7ddd4da9b0da5fc326017122
Derek Marion <derek.j.marion@gmail.com>
2023-10-05 08:28:26 -0700

    Reformat armstrong_numbers.py for readability

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -3,7 +3,9 @@ def find_armstrong_numbers(numbers):
     output = []
     for number in numbers:
         num_digits = len([num for num in str(number)])
-        sum_powers = sum(pow(number, num_digits) for number in (int(num) for num in str(number)))
+        sum_powers = sum(
+            pow(number, num_digits) for number in (int(num) for num in str(number))
+        )
         if sum_powers == number:
             output.append(number)
     return output
