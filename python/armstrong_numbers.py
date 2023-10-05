cbb2ff59f12963e045b34940f4bb73478a656dbe
Derek Marion <derek.j.marion@gmail.com>
2023-10-05 08:25:10 -0700

    First commit for working python func that passes all tests

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,9 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    output = []
+    for number in numbers:
+        num_digits = len([num for num in str(number)])
+        sum_powers = sum(pow(number, num_digits) for number in (int(num) for num in str(number)))
+        if sum_powers == number:
+            output.append(number)
+    return output
